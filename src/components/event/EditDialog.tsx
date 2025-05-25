cf38b465f7e8eefab51dec20aa984feae6bd517d
khalifahnur <khalifahnur1095@gmail.com>
2025-05-25 12:07:51 +0300

    feat:decouple component delete,preview & edit

---
+++ src/components/event/EditDialog.tsx
@@ -0,0 +1,155 @@
+"use client"
+import { useState } from "react";
+import { EventData } from "../../../types/event";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+
+
+export default function EditItemDialog({
+  isOpen,
+  onClose,
+  item,
+  onEditItem,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  item: EventData;
+  onEditItem: (updatedItem: EventData, itemId: string) => void;
+}) {
+  const [editedItem, setEditedItem] = useState<EventData>(item);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onEditItem(editedItem, item._id);
+    onClose();
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit Event</DialogTitle>
+          <DialogDescription>
+            Make changes to the event here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="grid gap-4 py-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="space-y-2">
+              <Label htmlFor="date">Date</Label>
+              <Input
+                id="date"
+                name="date"
+                type="date"
+                value={editedItem.date}
+                onChange={(e) =>
+                  setEditedItem({ ...editedItem, date: e.target.value })
+                }
+                min={new Date().toISOString().split("T")[0]}
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="time">Time</Label>
+              <Input
+                id="time"
+                name="time"
+                type="time"
+                value={editedItem.time}
+                onChange={(e) =>
+                  setEditedItem({ ...editedItem, time: e.target.value })
+                }
+                required
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="homeTeam" className="text-right">
+              Home Team
+            </Label>
+            <Input
+              id="homeTeam"
+              value={editedItem.homeTeam}
+              onChange={(e) =>
+                setEditedItem({ ...editedItem, homeTeam: e.target.value })
+              }
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="awayTeam" className="text-right">
+              Away Team
+            </Label>
+            <Input
+              id="awayTeam"
+              value={editedItem.awayTeam}
+              onChange={(e) =>
+                setEditedItem({ ...editedItem, awayTeam: e.target.value })
+              }
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="edit-venue" className="text-right">
+              Venue
+            </Label>
+            <Input
+              id="edit-venue"
+              value={editedItem.venue}
+              onChange={(e) =>
+                setEditedItem({ ...editedItem, venue: e.target.value })
+              }
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="edit-price" className="text-right">
+              Ticket Price
+            </Label>
+            <Input
+              id="edit-price"
+              type="number"
+              value={editedItem.ticketPrice}
+              onChange={(e) =>
+                setEditedItem({
+                  ...editedItem,
+                  ticketPrice: Number(e.target.value),
+                })
+              }
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="edit-tickets" className="text-right">
+              Total Tickets
+            </Label>
+            <Input
+              id="edit-tickets"
+              type="number"
+              value={editedItem.totalTickets}
+              onChange={(e) =>
+                setEditedItem({
+                  ...editedItem,
+                  totalTickets: Number(e.target.value),
+                })
+              }
+              className="col-span-3"
+            />
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ src/components/event/teamLogo.ts
@@ -0,0 +1,89 @@
+const teamLogoUrl = [
+  {
+    id: 1,
+    team: "Ulinzi Star",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097295/ulinzi-fc_gwk5px.png",
+  },
+  {
+    id: 2,
+    team: "Shabana Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097294/shabana-fc_u2trlq.png",
+  },
+  {
+    id: 3,
+    team: "Tusker Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097294/tusker-fc_cnlyrd.png",
+  },
+  {
+    id: 4,
+    team: "Gor Mahia Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097293/gor-fc_yaylpj.png",
+  },
+  {
+    id: 5,
+    team: "Mathare United",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097293/mathare-united_ehtjuk.png",
+  },
+  {
+    id: 6,
+    team: "Talanta Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097292/fc-talanta_ifnlx9.png",
+  },
+  {
+    id: 7,
+    team: "Kariobangi Sharks",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097292/kariobangi-sharks_cals5v.png",
+  },
+  {
+    id: 8,
+    team: "Kenya Police Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097291/kenya-police_btrcyb.png",
+  },
+  {
+    id: 9,
+    team: "Bidco United",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097290/bidco-united_zkankj.png",
+  },
+  {
+    id: 10,
+    team: "Bandari Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097290/bandari-fc_dsfjg5.png",
+  },
+  {
+    id: 11,
+    team: "Posta Rangers Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097289/posta-rangers_vkqumx.png",
+  },
+  {
+    id: 12,
+    team: "AFC Leaopards",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097288/fc-leopards_j0usxp.png",
+  },
+  {
+    id: 13,
+    team: "Sofapaka Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097288/sofapaka_gt9gre.png",
+  },
+  {
+    id: 14,
+    team: "Kakamega Homeboyz",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097287/kakamega-homeboyz_ykdgen.png",
+  },
+  {
+    id: 15,
+    team: "Mara Sugar",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097287/mara-sugar_kvvvir.png",
+  },
+  {
+    id: 16,
+    team: "KCB Fc",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097287/kcb-fc_cm0hzj.png",
+  },
+  {
+    id: 17,
+    team: "Muranga Seal",
+    logoUrl: "https://res.cloudinary.com/dfuh1q6ic/image/upload/v1748097286/mseal_yqyp5v.png",
+  },
+]
+
+export default teamLogoUrl
